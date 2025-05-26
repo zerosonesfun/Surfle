a4281058100b2a73df94a163ec20be1f6819168e
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2025-05-26 18:58:22 -0400

    Update content.js
    Add click support

--- extension/content.js
+++ extension/content.js
@@ -1,15 +1,20 @@
-let tapCount=0,tapTimeout;
-document.addEventListener('keydown', e => {
-  if(['INPUT','TEXTAREA'].includes(e.target.tagName) || e.isComposing) return;
-  if(e.key==='ArrowRight') chrome.runtime.sendMessage({ action: 'goNext' });
-  if(e.key==='ArrowLeft') chrome.runtime.sendMessage({ action: 'goPrev' });
-});
-document.addEventListener('touchstart', () => {
+let tapCount = 0, tapTimeout;
+
+function handleTapLikeAction() {
   tapCount++;
   clearTimeout(tapTimeout);
   tapTimeout = setTimeout(() => {
-    if(tapCount===3) chrome.runtime.sendMessage({ action: 'goNext' });
-    if(tapCount===4) chrome.runtime.sendMessage({ action: 'goPrev' });
-    tapCount=0;
+    if (tapCount === 3) chrome.runtime.sendMessage({ action: 'goNext' });
+    if (tapCount === 4) chrome.runtime.sendMessage({ action: 'goPrev' });
+    tapCount = 0;
   }, 500);
+}
+
+document.addEventListener('keydown', e => {
+  if (['INPUT', 'TEXTAREA'].includes(e.target.tagName) || e.isComposing) return;
+  if (e.key === 'ArrowRight') chrome.runtime.sendMessage({ action: 'goNext' });
+  if (e.key === 'ArrowLeft') chrome.runtime.sendMessage({ action: 'goPrev' });
 });
+
+document.addEventListener('touchstart', handleTapLikeAction);
+document.addEventListener('click', handleTapLikeAction);
